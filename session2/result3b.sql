b2d06c9ee38456580d6c6525e0ab27359cd02b63
Michiel te Braake <m.tebraake.95@gmail.com>
2014-05-06 17:22:32 +0200

    3b - shunting

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,10 +1,7 @@
-SELECT p.pid FROM Person p 
-WHERE EXISTS( 
-SELECT * FROM Acts a 
-WHERE a.pid = p.pid 
-AND EXISTS ( 
-SELECT * FROM Movie m  –­ the movies exists and a plays in it
-WHERE m.mid = a.mid 
-AND m.name = 'Back to the Future' 
-) 
-); 
+SELECT p.pid FROM Person p
+WHERE EXISTS (
+    SELECT * FROM Acts a, Movie m
+    WHERE a.pid = p.pid
+    AND m.mid = a.mid
+    AND m.name = 'Back to the Future'
+)
